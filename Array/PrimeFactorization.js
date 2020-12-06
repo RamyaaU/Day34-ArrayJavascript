ac57366672302062bbe1738cd259d36f64c6d8b6
Ramya.U <ramyayadav2016@gmail.com>
2020-12-06 14:30:12 +0530

    [Ramya] Refactor . Modified primefactorization.js

--- Array/PrimeFactorization.js
+++ Array/PrimeFactorization.js
@@ -5,7 +5,7 @@ io= require('console-read-write');
 //async makes the function execute when called and wait for user input statement
 //if async is not defined then await can not be defined and all user inputs will be taken together
 let primeFactorsArray= new Array();
-async function main()
+async function PrimeFactorization()
 {
     let checkForFactors= false;
     console.log("Please enter the first no. to get prime factors");
@@ -62,4 +62,4 @@ function prime(n)
         //console.log(n+" is a prime number.");
     }
 }
-main();
+PrimeFactorization();
